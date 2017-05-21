d45de3296b119602c8f1b0bcb7143024383a5b6f
Sebastian Kubalski <sebastian.kubalski@gmail.com>
2017-05-21 14:19:35 +0200

    —created forgotten password component

--- src/containers/ForgottenPassword.js
+++ src/containers/ForgottenPassword.js
@@ -5,11 +5,58 @@
  * @Description
  */
 import React from "react";
+import BackgroundImage from "../components/BackgroundImage";
+import InputEmail from "../components/InputEmail";
+import SubmitButton from "../components/SubmitButton";
+import {Link} from "react-router-dom";
 
 class ForgottenPassword extends React.Component {
+    state = {
+        email: '',
+        isEmailValid: true
+    };
+
+    constructor(props) {
+        super(props);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.remindPassword = this.remindPassword.bind(this);
+    }
+
+    remindPassword() {
+        // todo: logic to remind password
+        this.props.history.push('/forgotten-password/confirm');
+    }
+
+    handleEmailChange(email, isEmailValid) {
+        this.setState({
+            email,
+            isEmailValid
+        });
+    }
+
     render() {
         return (
-            <div>Forgotten Password</div>
+            <BackgroundImage title="Forgotten Password">
+                <div className="container">
+                    <form onSubmit={this.remindPassword}>
+                        <InputEmail
+                            name="Email"
+                            placeholder="Email"
+                            autoFocu
+                            handleChange={this.handleEmailChange}
+                        />
+                        <SubmitButton name="Sign in" disabled={!this.state.isEmailValid}/>
+                    </form>
+                    <div className="row">
+                        <div className="col-6">
+                            <Link to="/register" className="btn btn-link">Sign up</Link>
+                        </div>
+                        <div className="col-6">
+                            <Link to="/login" className="btn btn-link">Sign in</Link>
+                        </div>
+                    </div>
+                </div>
+            </BackgroundImage>
         );
     }
 }
